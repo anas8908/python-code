eb494bc180156ebd2b5f31d2bd0a1b9980537475
anas8908 <65181453+anas8908@users.noreply.github.com>
2020-05-14 15:18:31 +0100

    python

---
+++ do_if.py
@@ -0,0 +1,15 @@
+
+age = input('enter your age: ')
+if age = 18 or 19 or 20 or 21.
+    print('adult')
+if age = 10 or 11 or 12 or 13 or 14 or 15 or 16 or 17.
+    print('teenager')
+if age =5 or 6 or 7 or 8 or 9.
+    print('kid')
+enter =("do you want to exit? press enter")
+print("good "+enter)
+
+
+
+#aunty if i enter my age is between 18 to 21 it should show adult but it is showing error fix this.
+#delete this after editing.
